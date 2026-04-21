ac2f24969222006f39ea287f32b29146a7a821fd
Steffan Andrews <stef@orchetect.com>
2026-04-20 20:15:59 -0700

    Added SwiftUI Multiplatform - VirtualInput example

---
+++ Examples/IO/SwiftUI Multiplatform/VirtualInput/VirtualInput/VirtualInputApp.swift
@@ -0,0 +1,19 @@
+//
+//  VirtualInputApp.swift
+//  MIDIKit • https://github.com/orchetect/MIDIKit
+//  © 2021-2025 Steffan Andrews • Licensed under MIT License
+//
+
+import SwiftUI
+
+@main
+struct VirtualInputApp: App {
+    @State private var midiHelper = MIDIHelper(start: true)
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .environment(midiHelper)
+    }
+}
