946fcb67ea293b7c425bbd4762ad78255d5e42ee
Steffan Andrews <stef@orchetect.com>
2026-04-20 20:11:15 -0700

    Added SwiftUI Multiplatform - EndpointPickers example

---
+++ Examples/IO/SwiftUI Multiplatform/EndpointPickers/EndpointPickers/EndpointPickersApp.swift
@@ -0,0 +1,21 @@
+//
+//  EndpointPickersApp.swift
+//  MIDIKit • https://github.com/orchetect/MIDIKit
+//  © 2021-2025 Steffan Andrews • Licensed under MIT License
+//
+
+import Combine
+import SwiftMIDIIO
+import SwiftUI
+
+@main
+struct EndpointPickersApp: App {
+    @State var midiHelper = MIDIHelper(start: true)
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .environment(midiHelper)
+    }
+}
