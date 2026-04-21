b34eca1fc1970b009b7060d85e15a50bfc4fba90
Steffan Andrews <stef@orchetect.com>
2026-04-20 20:06:11 -0700

    Added SwiftUI iOS - BluetoothMIDI example

---
+++ Examples/IO/SwiftUI iOS/BluetoothMIDI/BluetoothMIDI/BluetoothMIDIApp.swift
@@ -0,0 +1,20 @@
+//
+//  BluetoothMIDIApp.swift
+//  MIDIKit • https://github.com/orchetect/MIDIKit
+//  © 2021-2025 Steffan Andrews • Licensed under MIT License
+//
+
+import SwiftMIDIIO
+import SwiftUI
+
+@main
+struct BluetoothMIDIApp: App {
+    @State var midiHelper = MIDIHelper(start: true)
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .environment(midiHelper)
+    }
+}
