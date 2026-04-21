53b52d6ae7556e40b290039574da6a32f2ae2e26
Steffan Andrews <stef@orchetect.com>
2026-04-20 19:54:15 -0700

    Added Control Surfaces - HUITest example

---
+++ Examples/Control Surfaces/HUITest/HUITest/Control Views/SwiftUI Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftUI Extensions.swift
+//  MIDIKit • https://github.com/orchetect/MIDIKit
+//  © 2021-2025 Steffan Andrews • Licensed under MIT License
+//
+
+import SwiftUI
+
+extension View {
+    func spaceHogFrame(alignment: Alignment = .center) -> some View {
+        ZStack(alignment: alignment) {
+            Color.clear
+            self
+        }
+    }
+}
